c3c1d8f48b88672ad53be43bf496d26113eae5c6
PayalNagar14 <payalnagar317@gmail.com>
2024-10-09 18:38:03 +0530

    Update android webcam.py

--- android webcam.py
+++ android webcam.py
@@ -1,14 +1,11 @@
-
-# Import essential libraries
 import requests
 import cv2
 import numpy as np
 import imutils
   
-# Replace the below URL with your own. Make sure to add "/shot.jpg" at last.
+# Your URL.add "/shot.jpg" at last.
 url = "http://192.0.0.4:8080/shot.jpg"
   
-# While loop to continuously fetching data from the Url
 while True:
     img_resp = requests.get(url)
     img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
@@ -16,7 +13,6 @@ while True:
     img = imutils.resize(img, width=1000, height=1800)
     cv2.imshow("Android_cam", img)
   
-    # Press Esc key to exit
     if cv2.waitKey(1) == 27:
         break
   
